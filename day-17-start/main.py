d0ecb8e8adc7359d77b69a7ab7a57448f6211e54
KimHyunWoo0102 <rlagusdn60@gmail.com>
2024-11-26 21:42:52 +0900

    Implements : day-17:quiz project

---
+++ day-17-start/main.py
@@ -0,0 +1,10 @@
+class User:
+    x=10
+    def __init__(self):
+        self.x=5
+        print("hi")
+user_1=User()
+
+print(user_1.x)
+def fuction():
+    pass
---
+++ day-17-start/quiz-game/question_model.py
@@ -0,0 +1,7 @@
+
+
+class Question :
+    def __init__(self,text,answer):
+        """text and answer initialize"""
+        self.text=text
+        self.answer=answer
