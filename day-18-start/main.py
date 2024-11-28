5d2846a6f7a3f1d3f5977201e15ecae69f703f1a
KimHyunWoo0102 <rlagusdn60@gmail.com>
2024-11-28 23:11:16 +0900

    Edited : using setheading not right

--- day-18-start/main.py
+++ day-18-start/main.py
@@ -3,7 +3,7 @@ import turtle as t
 import random
 
 timmy_the_turtle=t.Turtle()
-colours=["red","blue",'green','yellow','purple','coral']
+colours=["CornflowerBlue","DarkOrchid",'IndianRed','DeepSkyBlue','LightSeaGreen','wheat','SlateGray','SeaGreen']
 angles=[0,90,180,270]
 speeds=['fastest','fast','normal','slow','slowest']
 #timmy_the_turtle.shape("turtle")
@@ -12,7 +12,7 @@ speeds=['fastest','fast','normal','slow','slowest']
 timmy_the_turtle.width(10)
 while True:
     timmy_the_turtle.forward(random.randint(20,100))
-    timmy_the_turtle.right(random.choice(angles))
+    timmy_the_turtle.setheading(random.choice(angles))
     timmy_the_turtle.color(random.choice(colours))
     timmy_the_turtle.speed(random.choice(speeds))
 
